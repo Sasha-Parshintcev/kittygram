28b98437fecb269d612eb18e0891f19b5eb7ecc2
Саша Паршинцев <aleksandr-par@mail.ru>
2024-02-01 20:21:37 +0700

    API CRUD viewsets

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 
-from cats.views import CatViewSet
+from cats.views import CatViewSet, OwnerViewSet
 
 from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
-router.register('cats', CatViewSet, basename='tiger')
+router.register('cats', CatViewSet)
+router.register('owners', OwnerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
